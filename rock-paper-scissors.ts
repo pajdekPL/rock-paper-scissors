b04c17f1e600455ecff36a0459b8969853ac7b81
Adam Pajda <a.pajda@bonasoft.pl>
2024-07-23 20:50:00 +0200

    feat: add simply gui

--- rock-paper-scissors.ts
+++ rock-paper-scissors.ts
@@ -5,10 +5,17 @@ const WINNING_AGAINST = {
     PAPER: "ROCK",
     SCISSORS: "PAPER",
 }
+type Result = "Computer" | "User" | "Tie"
 
 export class RockPaperScissors {
     userScore = 0;
     computerScore = 0;
+    winningScore = 5;
+    scissorsButton = document.querySelector("#scissors")
+    rockButton = document.querySelector("#rock")
+    paperButton = document.querySelector("#paper")
+    scoreDiv = document.querySelector("#score")
+
     generateComputerMove(): MovesType {
         return MOVES[~~(Math.random() * MOVES.length)]
     }
@@ -30,19 +37,51 @@ export class RockPaperScissors {
             this.userScore += 1
         }
     }
-    play(): void {
+    addButtonsListeners() {
+        this.scissorsButton.addEventListener("click", () => this.playRound("SCISSORS"))
+        this.rockButton.addEventListener("click", () => this.playRound("ROCK"))
+        this.paperButton.addEventListener("click", () => this.playRound("PAPER"))
+
+    }
+    playRound(userMove: MovesType): void {
         const computerMove = this.generateComputerMove()
-        const userMove = this.takeUserInput()
-        console.log(`USER MOVE: ${userMove} vs COMPUTER MOVE: ${computerMove}`)
-        this.decideWinner(computerMove, userMove)
+        const winner = this.getWinner(userMove, computerMove)
+        if (winner === "Computer") this.computerScore += 1
+        if (winner === "User") this.userScore += 1
+        this.displayScore()
+        console.log(`Computer: ${this.computerScore} vs User ${this.userScore}`)
+    }
+    displayScore(){
+        if(this.computerScore < this.winningScore && this.userScore < this.winningScore) {
+            this.scoreDiv.textContent = `User score: ${this.userScore} vs Computer score: ${this.computerScore}`
+        }
+        else if(this.computerScore >= this.winningScore){
+            alert("Computer win!")
+            this.resetGame()
+        } else{
+            alert("User win!")
+            this.resetGame()
+        }
+    }
+
+    resetGame(){
+        this.computerScore = 0;
+        this.userScore = 0;
+        this.scoreDiv.textContent = ""
+    }
+
+    getWinner(userMove: MovesType, computerMove: MovesType): Result {
+        if (computerMove === userMove) {
+            return "Tie"
+        } else if (WINNING_AGAINST[computerMove] === userMove) {
+            return "Computer"
+        } else {
+            return "User"
+        }
     }
 }
 
 
 const game = new RockPaperScissors()
-let numberOfRounds = 3
-while(numberOfRounds > 0) {
-    game.play()
-    console.log(`Current score User vs Computer: ${game.userScore} vs ${game.computerScore}`)
-    numberOfRounds -= 1
-}
+game.addButtonsListeners()
+console.log(`Current score User vs Computer: ${game.userScore} vs ${game.computerScore}`)
